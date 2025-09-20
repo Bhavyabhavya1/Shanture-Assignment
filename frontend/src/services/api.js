6120a7b800e0a9971ce36e5400b99ae6d0ca6577
Samala bhavya <139446240+Bhavyabhavya1@users.noreply.github.com>
2025-09-20 20:19:35 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://shanture-assignment-backend-gws7.onrender.com/api',
 });
 
 export const fetchTopProducts = async () => {
